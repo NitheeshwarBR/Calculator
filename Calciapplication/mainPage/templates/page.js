c1da919c8ea6b29dde15e0041b24cd76d81541ea
NITHEESHWAR B R <nitheeshwar.r.g@gmail.com>
2022-10-22 09:52:10 +0530

    added all files properly

---
+++ Calciapplication/mainPage/templates/page.js
@@ -0,0 +1,8 @@
+let num1=document.getElementById('num').value;
+let num2=document.getElementById('num').value;
+let buttons=document.getElementsByClassName('btns').value;
+let screen=documet.getElementById('screenid');
+let operator=documet.getElementById('oper').value;
+buttons.addEventListener('click',function(){
+
+})
